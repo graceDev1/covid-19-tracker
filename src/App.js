b4edd98757e1846b1360b5acd9373b77bcb9706b
funcoder <grace.devolop@gmail.com>
2020-07-21 13:04:27 +0200

    create covid-tracker project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { 
+  FormControl, 
+  MenuItem,
+  Select
+} from '@material-ui/core';
 function App() {
+  // naming covention
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app_header"> 
+    <h1>COVID 19 TRACKER</h1>
+      {/* header */}
+      {/* Title * select input field */}
+      {/* infoBoxs */}
+      {/* infoBoxs */}
+      {/* infoBoxs */}
+
+      {/* table */}
+ 
     </div>
   );
 }
